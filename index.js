7a3f454ceae159a0f9e9cea1b1313dfeffeab198
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2022-02-24 09:59:03 +0530

    update

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ image2.setAttribute("src",randomDiceImage);
 
 if(randomNumber1>randomNumber2)
 {
-    document.querySelector("h1").innerHTML="Player 1 wins";
+    document.querySelector("h1").innerHTML="🚩Player 1 wins";
 }
 else if(randomNumber2>randomNumber1)
 {
-    document.querySelector("h1").innerHTML="Player 2 wins";
+    document.querySelector("h1").innerHTML="Player 2 wins🚩";
 }
 else
 {
-    document.querySelector("h1").innerHTML="Draw";
+    document.querySelector("h1").innerHTML="🚩Draw🚩";
 }
